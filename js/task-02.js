2ddf75fdf4d1fbe662ffb4ec59154218daa10934
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-01-28 21:27:54 +0200

    edit task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsUL = document.createElement("ul");
+
+const IngList = [];
+
+for (const ingredient of ingredients) {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  console.log(ingredientItem.textContent);
+  ingredientItem.classList.add("item");
+  IngList.push(ingredientItem);
+}
+
+ingredientsUL.append(...IngList);
+console.log(ingredientsUL);
+
