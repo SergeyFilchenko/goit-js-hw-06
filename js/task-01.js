acc706d7279795f88552800abe2976b8b7a5fb0d
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-01-28 20:52:12 +0200

    update t1

--- js/task-01.js
+++ js/task-01.js
@@ -3,12 +3,12 @@ const categories = document.querySelector("#categories");
 console.log(categories);
 
 const menuItems = categories.querySelectorAll(".item");
-console.log("Количество категорий:", menuItems.length);
+console.log("Number of categories:", menuItems.length);
 
 for( const menuItem of menuItems){
   const title = menuItem.querySelector("h2");
-  console.log("Категория:", title.textContent);
+  console.log("Category:", title.textContent);
 
   const element = menuItem.querySelectorAll("li");
-  console.log("Количество елементов:", element.length);
+  console.log("Elements:", element.length);
 }
