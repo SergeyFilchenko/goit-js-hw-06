30490ba2a6257e90a2bab1b29578ae330d182ef0
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-01-30 17:33:42 +0200

    task4+

--- js/task-04.js
+++ js/task-04.js
@@ -1,45 +1,25 @@
-// let counterValue = 0;
-// const counter = document.querySelector("#counter");
-// const value = document.querySelector("#value");
-
-
-// const decrement = counter.querySelector('[data-action="decrement"]');
-// const increment = counter.querySelector('[data-action="increment"]');
-
-
-const handleClickD = decrement.addEventListener("click", () => {
-    counterValue -=1;
-  console.log("Button decrement was clicked, counterValue = ", counterValue);
-  value.textContent = counterValue;
-  decrement.style.backgroundColor = 'red';
-  increment.style.backgroundColor = 'transparent';
-  decrement.style.borderRadius = '40%';
-
-});
-
-const handleClickI = increment.addEventListener("click", () => {
-    counterValue +=1;
-  console.log("Button increment was clicked, counterValue = ", counterValue);
-  value.textContent = counterValue;
-  decrement.style.backgroundColor = 'transparent';
-  increment.style.backgroundColor = 'green';
-  increment.style.borderRadius = '40%';
-
-});
 
 let counterValue = 0;
-const counter = document.querySelector('#counter');
-const value = document.querySelector('#value');
+const counter = document.querySelector("#counter");
+const value = document.querySelector("#value");
 
 const decrement = counter.querySelector('[data-action = "decrement"]');
-const decrement = counter.querySelector('[data-action = increment]');
+const increment = counter.querySelector('[data-action = "increment"]');
 
 const mouseClick = decrement.addEventListener("click", ()=>{
     counterValue -=1;
-    console.log("button was clicked", counterValue);
-    
+    console.log("button decrement was clicked", counterValue);
+    value.textContent = counterValue;
+    decrement.style.borderRadius = '50%';
 })
 
+const mouseClickI = increment.addEventListener("click", () =>{
+    counterValue +=1;
+    console.log("button increment was clicked", counterValue);
+    value.textContent = counterValue;
+    increment.style.borderRadius = '50%';
+   
+})
 
 
 
