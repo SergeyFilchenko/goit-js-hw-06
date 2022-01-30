90783734029546152c336ccb774dfadadacb1926
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-01-30 19:44:32 +0200

    task7

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const text = document.querySelector('#text');
+const fontSizeControl = document.querySelector('#font-size-control');
+
+
+fontSizeControl.addEventListener("input", (event)=>{
+    text.style.fontSize = `${event.target.value}px`;
+})
