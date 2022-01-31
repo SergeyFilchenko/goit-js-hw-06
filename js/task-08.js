609a2fbe2fb6e00d951728dbda911a034f49303e
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-01-31 18:00:30 +0200

    start8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  // const formElements = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Не все поля были запонены!");
+  }
+
+  const obj = {
+    email: form.email.value,
+    password: form.password.value,
+  };
+
+  console.log(obj);
+
+  form.reset();
+}
+
+
 
