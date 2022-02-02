4532035c131000fddbd43334605d231c915ab0d6
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-02-02 21:34:07 +0200

    update task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const randomColor = document.querySelector(".color");
+const bodyTarg = document.querySelector("body");
+const  changeBtn = document.querySelector(".change-color");
+
+changeBtn.addEventListener("click", getRandomHexColor);
+
+function getRandomHexColor(event) {
+  let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  bodyTarg.style.backgroundColor = color;
+  randomColor.textContent = `Текущий цвет: ${color}`;
+
 }
+ 
